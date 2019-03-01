4ca5b28c7a80452b091f442c8c60dde983d50ceb
Kévin Miri <miri.kevin@gmail.com>
2019-03-01 12:14:35 +0100

    search bar with countries

--- src/components/Search.js
+++ src/components/Search.js
@@ -15,11 +15,11 @@ class Search extends Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    console.log("it works i guess?");
+    console.log("handleSubmit ok ?");
   }
 
   genericOnChange(event) {
-    console.log(event.target.value, "here is the event");
+    console.log(event.target.value, "TEST generic onchange event");
     const { name, value } = event.target;
     this.setState({ [name]: value });
   }
@@ -29,13 +29,20 @@ class Search extends Component {
     // get datat from our Express API (localhost:5555) (now in api.js)
     getCountries().then(response => {
       //ALWAYS console.log () response.data to see what the API give you
-      console.log("our countries", response.data);
+      console.log("our COUNTRIES :", response.data);
       // SAVE the JSON data from the API into the state
       this.setState({ countryArray: response.data });
     });
   }
+
   render() {
-    const { countryArray } = this.state;
+    const { countryArray, countrySearch } = this.state;
+
+    const lowerSearch = countrySearch.toLowerCase();
+    const filteredArray = countryArray.filter(oneCountry => {
+      const lowerName = oneCountry.name.toLowerCase();
+      return lowerName.includes(lowerSearch);
+    });
 
     return (
       <section className="searchResult">
@@ -44,7 +51,7 @@ class Search extends Component {
         <form onSubmit={event => this.handleSubmit(event)}>
           <input
             onChange={event => this.genericOnChange(event)}
-            value={this.state.countrySearch}
+            value={this.state.oneCountry}
             name="countrySearch"
             type="text"
             className="search-bar"
@@ -53,12 +60,12 @@ class Search extends Component {
         </form>
 
         <ul>
-          {countryArray.map(oneCounrty => {
+          {filteredArray.map(oneCountry => {
             return (
-              <li key={oneCounrty._id}>
-                <h3>{oneCounrty.name}</h3>
-                <p>{oneCounrty.capital}</p>
-                <img src={oneCounrty.flag} />
+              <li key={oneCountry._id}>
+                <h3>{oneCountry.name}</h3>
+                <p>{oneCountry.capital}</p>
+                <img src={oneCountry.flag} />
               </li>
             );
           })}
