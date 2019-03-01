683c697e5358b5b5ccc6c9010be4a1241681cd69
Kévin Miri <miri.kevin@gmail.com>
2019-03-01 13:13:59 +0100

    try to récup

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -17,12 +17,13 @@ class HomePage extends Component {
           <h1>Link'All</h1>
           <h2>Welcome to Link'All ! Your favorite Chat counrtry.</h2>
 
-         
           {/* <button onClick={() => { SignupForm }}>Sign Up</button> */}
 
-          <SignupForm currentUser={this.props.currentUser} signupSuccess={this.props.signupSuccess} />
-          <LoginForm currentUser={this.props.currentUser} 
-          
+          <SignupForm
+            currentUser={this.props.currentUser}
+            signupSuccess={this.props.signupSuccess}
+          />
+          <LoginForm currentUser={this.props.currentUser} />
         </section>
 
         <section>
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,79 +1,72 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 import "./LoginForm.css";
-import { postLogin } from '../api';
-import { Redirect } from 'react-router-dom';
+import { postLogin } from "../api";
+import { Redirect } from "react-router-dom";
 
 class LoginForm extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      email: "",
+      originalPassword: ""
+    };
+  }
 
-    constructor(props) {
-        super(props);
+  genericOnChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
 
-        this.state = {
-            email: "",
-            originalPassword: "",
+  handleSubmit(event) {
+    event.preventDefault();
 
-        };
-    }
+    //submit login into the backend
+    postLogin(this.state).then(response => {
+      //console.log("Log In Result", response.data);
+      this.props.loginSuccess(response.data);
+    });
+  }
 
-    genericOnChange(event) {
-        const { name, value } = event.target;
-        this.setState({ [name]: value });
-    }
+  render() {
+    return this.props.currentUser ? (
+      // currentUser is now sent by App.js as a prop
+      <Redirect to="/countries" />
+    ) : (
+      <section className="LoginForm">
+        <h2>You are at few steps from joining the LinkAll community</h2>
 
-    handleSubmit(event) {
-        event.preventDefault();
+        <form onSubmit={event => this.handleSubmit(event)}>
+          <img src={currentUser.profileImg} />
 
-        //submit login into the backend
-        postLogin(this.state).then(response => {
-            //console.log("Log In Result", response.data);
-            this.props.loginSuccess(response.data);
-        });
-    }
+          <label>
+            Email:
+            <input
+              onChange={event => this.genericOnChange(event)}
+              value={this.state.email}
+              name="email"
+              type="email"
+              placeholder="Your email address"
+            />
+          </label>
 
+          <label>
+            Password:
+            <input
+              onChange={event => this.genericOnChange(event)}
+              value={this.state.originalPassword}
+              name="originalPassword"
+              type="password"
+              placeholder="Your password"
+            />
+          </label>
 
-
-    render() {
-        return this.props.currentUser ? (
-            // currentUser is now sent by App.js as a prop
-            <Redirect to="/countries" />
-        )
-            :
-            (
-                <section className="LoginForm">
-                    <h2>You are at few steps from joining the LinkAll community</h2>
-
-                    <form onSubmit={event => this.handleSubmit(event)}>
-                        <img src={currentUser.profileImg}/>
-
-                        <label>
-                            Email:
-                        <input
-                                onChange={event => this.genericOnChange(event)}
-                                value={this.state.email}
-                                name="email"
-                                type="email"
-                                placeholder="Your email address" />
-                        </label>
-
-                        <label>
-                            Password:
-                        <input
-                                onChange={event => this.genericOnChange(event)}
-                                value={this.state.originalPassword}
-                                name="originalPassword"
-                                type="password"
-                                placeholder="Your password" />
-                        </label>
-
-                        <button>Log In</button>
-                    </form>
-                </section>
-            );
-    }
+          <button>Log In</button>
+        </form>
+      </section>
+    );
+  }
 }
-
-
 
 export default LoginForm;
