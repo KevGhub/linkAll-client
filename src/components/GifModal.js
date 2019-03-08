0e9f407e558a437c36568e041734097c0bc1abe0
Deie <chantalsauvann@gmail.com>
2019-03-08 16:10:12 +0100

    css

--- src/components/GifModal.js
+++ src/components/GifModal.js
@@ -11,8 +11,8 @@ const GifModal = (props) => {
             isOpen={props.modalIsOpen}
             onRequestClose={() => props.onRequestClose()}>
             <div className="gif-modal">
-                <img src={props.selectedGif.images.downsized.url} />
-                <button
+                <img className="gifimg" src={props.selectedGif.images.downsized.url} />
+                <button className="btn btn-primary btnviolet"
                     onClick={() => {
                         console.log("AAAAAAAAAAAAAAAAAA", props.selectedGif.images.downsized.url );
                      return props.onRequestClose(props.selectedGif.images.downsized.url)}}>Send</button>
